1ac3528b196f2a58bc98e6c507a1982be1e8db4c
Darlan Thiago <36196918+darlanthiago@users.noreply.github.com>
2020-03-25 16:43:45 -0300

    Backend add

---
+++ backend/src/routes.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const routes = express.Router();
+
+//controllers
+const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
+
+
+routes.post('/sessions', SessionController.create);
+
+routes.post('/ongs', OngController.store);
+routes.get('/ongs', OngController.index);
+
+routes.get('/profile', ProfileController.index);
+
+routes.post('/incidents', IncidentController.store);
+routes.get('/incidents', IncidentController.index);
+routes.delete('/incidents/:id', IncidentController.delete);
+
+module.exports = routes;
